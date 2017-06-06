768bd626747efebf22221e6389a8b174c4ca17f9
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-06-06 09:10:09 -0600

    formulating setup for test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,15 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
-
+  attr_reader :pantry
+  def setup
+   @pantry = Pantry.new
+  end
+# => <Pantry...>
+  def test_stock_prints_stock_as_hash
+    result = pantry.stock
+    expected = {}
+    assert_instance_of Hash, result
+    assert_equal expected, result
+  end
 end
